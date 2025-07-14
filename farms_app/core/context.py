4eae948e7720e22841c0e27bcab373eee05d08b2
Shravan Tata <shravantr@gmail.com>
2025-07-13 21:44:03 -0400

    [CORE] Added application context module

---
+++ farms_app/core/context.py
@@ -0,0 +1,14 @@
+""" Context
+
+Maintains the current running context of the entire application, windows, ...
+
+"""
+
+
+class ApplicationContext:
+    """ Application Context """
+
+
+
+class WindowContext:
+    """ Window Context """
