f664530ebe742685a7959babb534fbbbe090df7c
Shravan Tata <shravantr@gmail.com>
2025-08-11 15:08:52 -0400

    [SCRATCH] Added test for passing options to applications

--- scratch/run.py
+++ scratch/run.py
@@ -2,8 +2,12 @@
 
 from farms_core import pylog
 from farms_app.core.application import FARMSApplication
+from farms_app.core.options import ApplicationOptions
 
 pylog.set_level("debug")
 
-app = FARMSApplication()
+
+app_options = ApplicationOptions(title="FARMS-APP")
+
+app = FARMSApplication.from_options(app_options)
 app.run()
