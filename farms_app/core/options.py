76212b35a979dda99165df1c87b4e7d54c07aed9
Shravan Tata <shravantr@gmail.com>
2025-09-24 23:52:13 -0400

    [CORE][WIP] Added basic structure for options

--- farms_app/core/options.py
+++ farms_app/core/options.py
@@ -1,12 +1,23 @@
+""" Options for FARMS Application """
+
+from enum import StrEnum
 from typing import Any, Dict, Iterable, List, Self, Type, Union
 
+from farms_app.backends.manager import PlatformType, RendererType
 from farms_core.options import Options
 
 
 class ApplicationOptions(Options):
     """ Application options """
 
-    def __init__(self, **kwargs):
+    def __init__(
+            self,
+            backend_options: 'BackendOptions',
+            window_options: 'WindowOptions',
+            docking_options: 'DockingOptions',
+            extension_options: 'ExtensionOptions',
+            **kwargs
+    ):
         "Initialize"
         super().__init__()
 
@@ -14,23 +25,54 @@ class ApplicationOptions(Options):
         self.title = kwargs.pop("title", "FARMS")  # FARMS by default
         self.geometry: Iterable[int, int] = kwargs.pop("geometry", [720, 1080])
         self.resizable: bool = kwargs.pop("resizable", True)
-        self.plugin_paths: List[str] = []        # Paths to search for the plugins
-        self.backend: str = None
 
         # Imgui Ini options. Imgui uses ini files to save and restore the state of the
         # app and this can be used in conjunction to the FARMS options to either
         # override or save use preferences.
 
+        # Backend options
+        self.backend = backend_options
+
         # Window options
+        self.window = window_options
 
         # Docking options
+        self.docking = docking_options
 
-        # Backends
+        # Extension options
+        self.extension = extension_options
 
         # FPS
         self.fps = kwargs.pop("fps", 60.0)       # 60 FPS by default
 
         # Plot options
+
+
+class PlatformType(StrEnum):
+    """ Type of Platform """
+    GLFW = 'glfw'
+
+
+class RendererType(StrEnum):
+    """ Type of Renderer """
+    OPENGL2 = 'gl2'
+    OPENGL3 = 'gl3'
+
+
+class BackendOptions(Options):
+    """ Backend renderer options """
+
+    def __init__(self):
+        super().__init__()
+        self.platform = PlatformType.GLFW.value
+        self.renderer = RendererType.OPENGL2.value
+
+
+class PlatformOptions(Options):
+    """ Base class for different types of platform options """
+
+    def __init__(self):
+        ...
 
 
 class WindowOptions(Options):
@@ -51,10 +93,19 @@ class DockingOptions(Options):
         self.layout_config = kwargs.pop("layout_config", None)
 
 
-class PluginOptions(Options):
-    """Plugin Options """
+class ExtensionOptions(Options):
+    """Extension Options """
 
     def __init__(self, **kwargs):
         super().__init__()
         self.auto_load = kwargs.pop("auto_load", True)
-        self.enabled = kwargs.pop("enabled", [])
+
+
+if __name__ == '__main__':
+    opts = ApplicationOptions(
+        BackendOptions(),
+        WindowOptions(),
+        DockingOptions(),
+        ExtensionOptions(),
+    )
+    opts.save("/tmp/app.yaml")
